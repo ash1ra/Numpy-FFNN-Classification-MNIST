8f495163ff65a8027f3935b2e94234c0ab2ffffd
ash1ra <ash1rawtf@gmail.com>
2025-03-13 10:52:41 +0200

    feat: implement one_hot_encoding function
    feat: add type hints

--- main.py
+++ main.py
@@ -4,9 +4,16 @@ import pandas as pd
 TEST_DATA_SPLIT = 2000
 
 
-def get_data():
+def one_hot_encoding(y: np.ndarray) -> np.ndarray:
+    one_hot_y = np.zeros((y.size, np.max(y) + 1))
+    for one_hot_y_element, y_index in zip(one_hot_y, y):
+        one_hot_y_element[y_index] = 1
+    return one_hot_y
+
+
+def get_data() -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     data = pd.read_csv("data/train.csv").to_numpy()
-    np.random.shuffle(data) 
+    np.random.shuffle(data)
 
     train_data = data[TEST_DATA_SPLIT:].T
     test_data = data[:TEST_DATA_SPLIT].T
@@ -14,13 +21,15 @@ def get_data():
     x_train, y_train = train_data[1:], train_data[0]
     x_test, y_test = test_data[1:], test_data[0]
 
+    y_train = one_hot_encoding(y_train)
+    y_test = one_hot_encoding(y_test)
+
     return x_train, y_train, x_test, y_test
 
 
 def main():
-    get_data()
+    x_train, y_train, x_test, y_test = get_data()
 
 
 if __name__ == "__main__":
     main()
-
