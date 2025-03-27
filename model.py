28e29bb4f393c1b5f2d31d6480b2b5be204ce720
ash1ra <ash1rawtf@gmail.com>
2025-03-27 12:10:22 +0200

    refactor: change logger format, add validation data printing, change activation functions and optimizers usage format

--- model.py
+++ model.py
@@ -1,8 +1,9 @@
 import logging
 from time import perf_counter
+from typing import Callable
 
 import numpy as np
-from pydantic import BaseModel, ConfigDict, Field, PositiveFloat, PositiveInt
+from pydantic import BaseModel, ConfigDict, Field, NonNegativeFloat, PositiveFloat, PositiveInt
 
 INPUT_SIZE = 28 * 28
 OUTPUT_SIZE = 10
@@ -67,18 +68,18 @@ class Cache(BaseModel):
 
 class ModelData(BaseModel):
     hidden_neurons_count: PositiveInt
-    hidden_activation_func: str
-    optimizer: str
+    hidden_activation_func: tuple[Callable, Callable]
+    optimizer: Callable
     learning_rate: PositiveFloat
-    epochs: int
-    batch_size: int
+    epochs: PositiveInt
+    batch_size: PositiveInt
+    patience: PositiveInt
+    min_delta: NonNegativeFloat
     train_time: float | np.float64 = Field(default_factory=float)
     train_loss: list = Field(default_factory=list)
     train_accuracy: list = Field(default_factory=list)
     val_loss: list = Field(default_factory=list)
     val_accuracy: list = Field(default_factory=list)
-    patience: int
-    min_delta: float
     test_loss: np.float64 = Field(default_factory=np.float64)
     test_accuracy: np.float64 = Field(default_factory=np.float64)
 
@@ -88,20 +89,34 @@ class ModelData(BaseModel):
 class Model:
     def __init__(
         self,
-        hidden_neurons_count: int,
+        hidden_neurons_count: PositiveInt,
         hidden_activation_func: str,
         optimizer: str,
-        learning_rate: float,
-        epochs: int,
-        batch_size: int,
-        patience: int = 10,
-        min_delta: float = 0.0,
+        learning_rate: PositiveFloat,
+        epochs: PositiveInt,
+        batch_size: PositiveInt,
+        patience: PositiveInt = 10,
+        min_delta: PositiveFloat = 0.0,
     ) -> None:
-        self.t = 0
+        self._activation_map = {
+            "sigmoid": (self._sigmoid, self._sigmoid_derivative),
+            "tanh": (self._tanh, self._tanh_derivative),
+            "relu": (self._relu, self._relu_derivative),
+            "leaky_relu": (self._leaky_relu, self._leaky_relu_derivative),
+            "elu": (self._elu, self._elu_derivative),
+        }
+        self._optimizer_map = {
+            "gradient_descent": self._gradient_descent,
+            "momentum": self._momentum,
+            "nesterov": self._nesterov,
+            "rmsprop": self._rmsprop,
+            "adam": self._adam,
+        }
+        self._t = 0
         self.model_data = ModelData(
             hidden_neurons_count=hidden_neurons_count,
-            hidden_activation_func=hidden_activation_func,
-            optimizer=optimizer,
+            hidden_activation_func=self._activation_map[hidden_activation_func],
+            optimizer=self._optimizer_map[optimizer],
             learning_rate=learning_rate,
             epochs=epochs,
             batch_size=batch_size,
@@ -113,20 +128,22 @@ class Model:
         self.grads: Grads
 
         if optimizer in ["momentum", "nesterov", "adam"]:
-            self.velocities = Velocities(
-                v_w1=np.zeros_like(self.params.w1),
-                v_b1=np.zeros_like(self.params.b1),
-                v_w2=np.zeros_like(self.params.w2),
-                v_b2=np.zeros_like(self.params.b2),
-            )
+            self.velocities = self._init_velocities()
 
         if optimizer in ["rmsprop", "adam"]:
-            self.cache = Cache(
-                s_w1=np.zeros_like(self.params.w1),
-                s_b1=np.zeros_like(self.params.b1),
-                s_w2=np.zeros_like(self.params.w2),
-                s_b2=np.zeros_like(self.params.b2),
-            )
+            self.cache = self._init_cache()
+
+        logger.info("Model initialization with parameters:")
+        logger.info(f"Number of hidden neurons: {self.model_data.hidden_neurons_count}")
+        logger.info(
+            f"Hidden layer activation function: {self.model_data.hidden_activation_func[0].__name__.replace('_', '').capitalize()}"
+        )
+        logger.info(f"Optimizer: {self.model_data.optimizer.__name__.replace('_', '').capitalize()}")
+        logger.info(f"Learning rate: {self.model_data.learning_rate}")
+        logger.info(f"Epochs: {self.model_data.epochs}")
+        logger.info(f"Batch size: {self.model_data.batch_size}")
+        logger.info(f"Patience: {self.model_data.patience}")
+        logger.info(f"Minimum delta: {self.model_data.min_delta}\n")
 
     def _init_params(self) -> Params:
         w1 = np.random.randn(INPUT_SIZE, self.model_data.hidden_neurons_count) * np.sqrt(2 / INPUT_SIZE)
@@ -138,6 +155,22 @@ class Model:
         b2 = np.zeros((1, OUTPUT_SIZE))
 
         return Params(w1=w1, b1=b1, w2=w2, b2=b2)
+
+    def _init_velocities(self) -> Velocities:
+        return Velocities(
+            v_w1=np.zeros_like(self.params.w1),
+            v_b1=np.zeros_like(self.params.b1),
+            v_w2=np.zeros_like(self.params.w2),
+            v_b2=np.zeros_like(self.params.b2),
+        )
+
+    def _init_cache(self) -> Cache:
+        return Cache(
+            s_w1=np.zeros_like(self.params.w1),
+            s_b1=np.zeros_like(self.params.b1),
+            s_w2=np.zeros_like(self.params.w2),
+            s_b2=np.zeros_like(self.params.b2),
+        )
 
     def _sigmoid(self, z: np.ndarray) -> np.ndarray:
         return 1 / (1 + np.exp(-z))
@@ -178,7 +211,7 @@ class Model:
 
     def _forward_prop(self, x: np.ndarray) -> None:
         z1 = np.matmul(x, self.params.w1) + self.params.b1
-        a1 = eval(f"self._{self.model_data.hidden_activation_func}(z1)")
+        a1 = self.model_data.hidden_activation_func[0](z1)
 
         z2 = np.matmul(a1, self.params.w2) + self.params.b2
         a2 = self._softmax(z2)
@@ -191,7 +224,7 @@ class Model:
         db2 = np.sum(dz2, axis=0, keepdims=True) / self.model_data.batch_size
 
         da1 = np.matmul(self.params.w2, dz2.T)
-        dz1 = da1.T * eval(f"self._{self.model_data.hidden_activation_func}_derivative(self.activations.z1)")
+        dz1 = da1.T * self.model_data.hidden_activation_func[1](self.activations.z1)
         dw1 = np.matmul(x.T, dz1) / self.model_data.batch_size
         db1 = np.sum(dz1, axis=0, keepdims=True) / self.model_data.batch_size
 
@@ -237,7 +270,7 @@ class Model:
         self.params.b2 -= self.model_data.learning_rate * self.grads.db2 / (np.sqrt(self.cache.s_b2) + EPS)
 
     def _adam(self, beta1: float = 0.9, beta2: float = 0.999):
-        self.t += 1
+        self._t += 1
 
         self.velocities.v_w1 = beta1 * self.velocities.v_w1 + (1 - beta1) * self.grads.dw1
         self.velocities.v_b1 = beta1 * self.velocities.v_b1 + (1 - beta1) * self.grads.db1
@@ -249,15 +282,15 @@ class Model:
         self.cache.s_w2 = beta2 * self.cache.s_w2 + (1 - beta2) * (np.power(self.grads.dw2, 2))
         self.cache.s_b2 = beta2 * self.cache.s_b2 + (1 - beta2) * (np.power(self.grads.db2, 2))
 
-        v_w1_corr = self.velocities.v_w1 / (1 - np.power(beta1, self.t))
-        v_b1_corr = self.velocities.v_b1 / (1 - np.power(beta1, self.t))
-        v_w2_corr = self.velocities.v_w2 / (1 - np.power(beta1, self.t))
-        v_b2_corr = self.velocities.v_b2 / (1 - np.power(beta1, self.t))
+        v_w1_corr = self.velocities.v_w1 / (1 - np.power(beta1, self._t))
+        v_b1_corr = self.velocities.v_b1 / (1 - np.power(beta1, self._t))
+        v_w2_corr = self.velocities.v_w2 / (1 - np.power(beta1, self._t))
+        v_b2_corr = self.velocities.v_b2 / (1 - np.power(beta1, self._t))
 
-        s_w1_corr = self.cache.s_w1 / (1 - np.power(beta2, self.t))
-        s_b1_corr = self.cache.s_b1 / (1 - np.power(beta2, self.t))
-        s_w2_corr = self.cache.s_w2 / (1 - np.power(beta2, self.t))
-        s_b2_corr = self.cache.s_b2 / (1 - np.power(beta2, self.t))
+        s_w1_corr = self.cache.s_w1 / (1 - np.power(beta2, self._t))
+        s_b1_corr = self.cache.s_b1 / (1 - np.power(beta2, self._t))
+        s_w2_corr = self.cache.s_w2 / (1 - np.power(beta2, self._t))
+        s_b2_corr = self.cache.s_b2 / (1 - np.power(beta2, self._t))
 
         self.params.w1 -= self.model_data.learning_rate * v_w1_corr / (np.sqrt(s_w1_corr) + EPS)
         self.params.b1 -= self.model_data.learning_rate * v_b1_corr / (np.sqrt(s_b1_corr) + EPS)
@@ -301,7 +334,7 @@ class Model:
 
                 self._forward_prop(x_batch)
                 self._backward_prop(x_batch, y_batch)
-                eval(f"self._{self.model_data.optimizer}()")
+                self.model_data.optimizer()
 
                 loss_per_epoch += self._cross_entropy(y_batch)
                 accuracy_per_epoch += self._calc_accuracy(y_batch)
@@ -322,8 +355,8 @@ class Model:
             self.model_data.val_accuracy.append(val_accuracy)
 
             logger.info(
-                f"Epoch: {epoch + 1} | Train Loss: {loss_per_epoch:.4f} | Train Acc: {(accuracy_per_epoch * 100):.2f}% | "
-                f"Val Loss: {val_loss:.4f} | Val Acc: {(val_accuracy * 100):.2f}%"
+                f"Epoch: {epoch + 1} | Train loss: {loss_per_epoch:.4f} | Train acc: {(accuracy_per_epoch * 100):.2f}% | "
+                f"Val loss: {val_loss:.4f} | Val acc: {(val_accuracy * 100):.2f}%"
             )
 
             if val_loss < best_val_loss - self.model_data.min_delta:
@@ -340,9 +373,10 @@ class Model:
                 logger.info(f"No improvement in val loss for {patience_counter}/{self.model_data.patience} epochs")
                 if patience_counter >= self.model_data.patience:
                     logger.info(f"Early stopping triggered after {epoch + 1} epochs")
-                    self.params = best_params  # Восстанавливаем лучшие веса
+                    self.params = best_params
                     break
-            self.model_data.train_time = perf_counter() - timer_start
+
+        self.model_data.train_time = perf_counter() - timer_start
 
     def test_model(self, x_test: np.ndarray, y_test: np.ndarray) -> None:
         self._forward_prop(x_test)
@@ -352,7 +386,7 @@ class Model:
         self.model_data.test_loss = loss
         self.model_data.test_accuracy = accuracy
 
-        logger.info(f"Test loss: {loss:.4f} | Test accuracy: {(accuracy * 100):.2f}%\n")
+        logger.info(f"Test loss: {loss:.4f} | Test acc: {(accuracy * 100):.2f}%\n")
 
     def calc_avarage(
         self,
@@ -367,33 +401,26 @@ class Model:
         all_train_times = []
         all_train_losses = []
         all_train_accuracies = []
+        all_val_losses = []
+        all_val_accuracies = []
         all_test_losses = []
         all_test_accuracies = []
 
         for i in range(count):
             logger.info(f"Run {i + 1}/{count}")
 
-            self.t = 0
+            self._t = 0
             self.params = self._init_params()
 
             if self.model_data.optimizer in ["momentum", "nesterov", "adam"]:
-                self.velocities = Velocities(
-                    v_w1=np.zeros_like(self.params.w1),
-                    v_b1=np.zeros_like(self.params.b1),
-                    v_w2=np.zeros_like(self.params.w2),
-                    v_b2=np.zeros_like(self.params.b2),
-                )
-
+                self.velocities = self._init_velocities()
             if self.model_data.optimizer in ["rmsprop", "adam"]:
-                self.cache = Cache(
-                    s_w1=np.zeros_like(self.params.w1),
-                    s_b1=np.zeros_like(self.params.b1),
-                    s_w2=np.zeros_like(self.params.w2),
-                    s_b2=np.zeros_like(self.params.b2),
-                )
+                self.cache = self._init_cache()
 
             self.model_data.train_loss = []
             self.model_data.train_accuracy = []
+            self.model_data.val_loss = []
+            self.model_data.val_accuracy = []
 
             self.train_model(x_train, y_train, x_val, y_val)
             self.test_model(x_test, y_test)
@@ -401,6 +428,8 @@ class Model:
             all_train_times.append(self.model_data.train_time)
             all_train_losses.append(self.model_data.train_loss)
             all_train_accuracies.append(self.model_data.train_accuracy)
+            all_val_losses.append(self.model_data.val_loss)
+            all_val_accuracies.append(self.model_data.val_accuracy)
             all_test_losses.append(self.model_data.test_loss)
             all_test_accuracies.append(self.model_data.test_accuracy)
 
@@ -415,12 +444,23 @@ class Model:
             avg_train_loss.append(np.mean(epoch_losses))
             avg_train_accuracy.append(np.mean(epoch_accuracies))
 
+        max_length = max(len(lst) for lst in all_val_losses)
+        avg_val_loss = []
+        avg_val_accuracy = []
+        for epoch in range(max_length):
+            epoch_losses = [lst[epoch] for lst in all_val_losses if len(lst) > epoch]
+            epoch_accuracies = [lst[epoch] for lst in all_val_accuracies if len(lst) > epoch]
+            avg_val_loss.append(np.mean(epoch_losses))
+            avg_val_accuracy.append(np.mean(epoch_accuracies))
+
         avg_test_loss = np.mean(all_test_losses)
         avg_test_accuracy = np.mean(all_test_accuracies)
 
         self.model_data.train_time = avg_train_time
         self.model_data.train_loss = avg_train_loss
         self.model_data.train_accuracy = avg_train_accuracy
+        self.model_data.val_loss = avg_val_loss
+        self.model_data.val_accuracy = avg_val_accuracy
         self.model_data.test_loss = avg_test_loss
         self.model_data.test_accuracy = avg_test_accuracy
 
@@ -430,5 +470,7 @@ class Model:
         )
         logger.info(f"Train loss: {avg_train_loss[-1]:.4f} (last epoch)")
         logger.info(f"Train accuracy: {(avg_train_accuracy[-1] * 100):.2f}% (last epoch)")
+        logger.info(f"Validation loss: {avg_val_loss[-1]:.4f} (last epoch)")
+        logger.info(f"Validation accuracy: {(avg_val_accuracy[-1] * 100):.2f}% (last epoch)")
         logger.info(f"Test loss: {avg_test_loss:.4f}")
         logger.info(f"Test accuracy: {(avg_test_accuracy * 100):.2f}%\n")
